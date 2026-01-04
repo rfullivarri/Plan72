bbedc604972123310c38909da73219674e5fe93c
rfullivarri <113726007+rfullivarri@users.noreply.github.com>
2026-01-04 21:53:19 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   // Ajusta estos valores si publicas en https://usuario.github.io/REPO
   output: "export",
-  basePath: "/REPO",
-  assetPrefix: "/REPO/",
+  basePath: "/Plan72/",
+  assetPrefix: "/Plan72/",
 };
 
 export default nextConfig;
